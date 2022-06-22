7a67ee83403a8441b1b7669fb64201e8c4d03512
Victorms4 <103613101+Victorms4@users.noreply.github.com>
2022-06-22 09:56:34 -0300

    teste dia 22/06

--- branchV.cpp
+++ branchV.cpp
@@ -2,6 +2,7 @@
 #include <graphics.h>
 #include <windows.h>
 #include <stdlib.h>
+#include <math.h>
 
 #define XMAX getmaxwidth()
 #define YMAX getmaxheight()
@@ -9,7 +10,8 @@
 typedef struct
 {
     char n[40];
-    int  v;
+    float v;
+    char j[40];
 } candidato;
 
   int primeirax1 = XMAX/16, primeiray1 = YMAX/16, primeirax2 = 3*XMAX/16, primeiray2 = 4*YMAX/16;
@@ -18,126 +20,313 @@ typedef struct
   int quartax1 = 10*XMAX/16, quartay1 = YMAX/16, quartax2 = 12*XMAX/16, quartay2 = 4*YMAX/16;
   int quintax1 = 13*XMAX/16, quintay1 = YMAX/16, quintax2 = 15*XMAX/16, quintay2 = 4*YMAX/16;
 
-int estado=-1, total=0, tela=1, vencedor;
+int estado=-1, total=0, tela=1, vencedorF, vencedorP;
+int pizzaF[6];
+int pizzaP[6];
+int porcentagemF[6];
+int porcentagemP[6];
+int i=0;
+int graus=360;
+int c=0;
 
-void clique(int *e, int *t, int *s, candidato f[]);
-int apuracao(int f[], int tamanho);
+void clique(int *e, int *t, int *s, int *v);
+int apuracao(candidato f[], int tamanho);
+void apagapalavra();
 
 int main()
 {
 
-  candidato f[5] = {"Harry Potter e a Ordem da Fenix", 0, "Gente Grande 2", 0, "Exemplo3", 0, "Exemplo4", 0, "Exemplo5", 0};
+  candidato f[5] = {"Harry Potter e a Ordem da Fenix", 0, "Harry Potter e a Ordem da Fenix.jpg",
+                    "Gente Grande 2", 0, "Gente Grande 2.jpg",
+                    "Exemplo3", 0, "Harry Potter e a pedra filosofal.jpg",
+                    "Exemplo4", 0,
+                    "Exemplo5", 0};
   
-  setbkcolor(15);
+  candidato g[5] = {"personagem1", 0,
+                    "personagem2", 0,
+                    "personagem3", 0,
+                    "personagem4", 0,
+                    "personagem5", 0};
+
+
   initwindow (XMAX,YMAX,"Project Lab AED-1",0,0);
-  readimagefile("Harry Potter e a Ordem da Fenix.jpg", primeirax1, primeiray1, primeirax2, primeiray2);
-  readimagefile("Gente Grande 2.jpg", segundax1, segunday1, segundax2, segunday2);
-  readimagefile("Exemplo3.jpg", terceirax1, terceiray1, terceirax2, terceiray2);
-  readimagefile("Exemplo4.jpg", quartax1, quartay1, quartax2, quartay2);
-  readimagefile("Exemplo5.jpg", quintax1, quintay1, quintax2, quintay2);
-  
-  outtextxy(primeirax1, (YMAX/16)+primeiray2, "Harry Potter e a Ordem da Fenix");
-  outtextxy(primeirax1, (YMAX/16)+segunday2, "Gente Grande 2");
-  outtextxy(primeirax1, (YMAX/16)+terceiray2, "Exemplo3");
-  outtextxy(primeirax1, (YMAX/16)+quartay2, "Exemplo4");
-  outtextxy(primeirax1, (YMAX/16)+quintay2, "Exemplo5");
+  setbkcolor(15);
 
-  setcolor(2);
-  bar(7*XMAX/16, 7*YMAX/16, 9*XMAX/16, 8*YMAX/16):
-  outtextxy(15*XMAX/32, 15*YMAX/32, "Confirmar");
-
-  setcolor(0);
-  rectangle(7*XMAX/16, 9*YMAX/16, 9*XMAX/16, 10*YMAX/16);
-  outtextxy(15*XMAX/32, 19*YMAX/32, "Branco");
-
-  outtextxy(XMAX/32, YMAX/32, "Escolha seu preferido:");
-
-  setcolor(4);
-  bar(29*XMAX/32, 29*YMAX/32, 31*XMAX/32, 30*YMAX/32);
-  outtextxy(59*XMAX/64, 59*YMAX/64, "Resultado");
-  
-  while(tela==1)
+  while(tela!=5)
   {
-    if (ismouseclick(WM_LBUTTONDOWN))
+    while(tela==1)
     {
-      clique(&estado, &total, &tela, f.v[]);
-      clearmouseclick(WM_LBUTTONDOWN);
+      cleardevice();
+      
+  //readimagefile("Harry Potter e a Ordem da Fenix.jpg", primeirax1, primeiray1, primeirax2, primeiray2);
+  //readimagefile("Gente Grande 2.jpg", segundax1, segunday1, segundax2, segunday2);
+  //readimagefile("Harry Potter e a pedra filosofal.jpg", terceirax1, terceiray1, terceirax2, terceiray2);
+  //readimagefile("Exemplo4.jpg", quartax1, quartay1, quartax2, quartay2);
+  //readimagefile("Exemplo5.jpg", quintax1, quintay1, quintax2, quintay2);
+      setfillstyle(1, 8);
+      bar(primeirax1, primeiray1, primeirax2, primeiray2);
+      bar(segundax1, segunday1, segundax2, segunday2);
+      bar(terceirax1, terceiray1, terceirax2, terceiray2);
+      bar(quartax1, quartay1, quartax2, quartay2);
+      bar(quintax1, quintay1, quintax2, quintay2);
+      
+      setcolor(0);
+      outtextxy(primeirax1, primeiray1, "personagem1");
+      outtextxy(segundax1, primeiray1, "personagem2");
+      outtextxy(terceirax1, primeiray1, "personagem3");
+      outtextxy(quartax1, primeiray1, "personagem4");
+      outtextxy(quintax1, primeiray1, "personagem5");
+    
+      setfillstyle(1,10);
+      setbkcolor(10);
+      bar(7*XMAX/16, 7*YMAX/16, 9*XMAX/16, 8*YMAX/16);
+      setcolor(0);
+      outtextxy(15*XMAX/31, 15*YMAX/32, "Confirmar");
+      setbkcolor(15);
+    
+      setcolor(0);
+      rectangle(7*XMAX/16, 9*YMAX/16, 9*XMAX/16, 10*YMAX/16);
+      outtextxy(15*XMAX/32, 19*YMAX/32, "Branco");
+    
+      outtextxy(XMAX/2, YMAX/32, "Escolha seu preferido:");
+    
+      setfillstyle(1,12);
+      setbkcolor(12);
+      bar(29*XMAX/32, 29*YMAX/32, 31*XMAX/32, 30*YMAX/32);
+      outtextxy(59*XMAX/64, 117*YMAX/128, "Resultado");
+      setbkcolor(15);
+      if (ismouseclick(WM_LBUTTONDOWN))
+      {
+        clique(&estado, &total, &tela, &g[estado].v);
+        clearmouseclick(WM_LBUTTONDOWN);
+      }
     }
+    while(tela==2)
+    {
+      cleardevice();
+      
+      readimagefile("Harry Potter e a Ordem da Fenix.jpg", primeirax1, primeiray1, primeirax2, primeiray2);
+      readimagefile("Gente Grande 2.jpg", segundax1, segunday1, segundax2, segunday2);
+      readimagefile("Harry Potter e a pedra filosofal.jpg", terceirax1, terceiray1, terceirax2, terceiray2);
+      readimagefile("Exemplo4.jpg", quartax1, quartay1, quartax2, quartay2);
+      readimagefile("Exemplo5.jpg", quintax1, quintay1, quintax2, quintay2);
+    
+      setcolor(0);
+      outtextxy(primeirax1, primeiray1, "Harry Potter e a Ordem da Fenix");
+      outtextxy(segundax1, primeiray1, "Gente Grande 2");
+      outtextxy(terceirax1, primeiray1, "Harry Potter e a Pedra Filosofal");
+      outtextxy(quartax1, primeiray1, "Exemplo4");
+      outtextxy(quintax1, primeiray1, "Exemplo5");
+    
+      setfillstyle(1,10);
+      setbkcolor(10);
+      bar(7*XMAX/16, 7*YMAX/16, 9*XMAX/16, 8*YMAX/16);
+      setcolor(0);
+      outtextxy(15*XMAX/31, 15*YMAX/32, "Confirmar");
+      setbkcolor(15);
+    
+      setcolor(0);
+      rectangle(7*XMAX/16, 9*YMAX/16, 9*XMAX/16, 10*YMAX/16);
+      outtextxy(15*XMAX/32, 19*YMAX/32, "Branco");
+    
+      outtextxy(XMAX/2, YMAX/32, "Escolha seu preferido:");
+      
+      if (ismouseclick(WM_LBUTTONDOWN))
+      {
+        clique(&estado, &total, &tela, &f[estado].v);
+        clearmouseclick(WM_LBUTTONDOWN);
+      }
+    }
+    while(tela==3)
+    {
+      cleardevice();
+      vencedorF = apuracao(f, 5);
+      vencedorP = apuracao(g, 5);
+    
+      outtextxy(XMAX/4, YMAX/2, "O vencedor dos personagens é:");
+      outtextxy(XMAX/4, 17*YMAX/32, g[vencedor].n);
+      readimagefile(g[vencedor].j, XMAX/4, 20*YMAX/32, 3*XMAX/8, 27*YMAX/32);
+    
+      outtextxy(3*XMAX/4, YMAX/2, "O vencedor dos filmes é:");
+      outtextxy(3*XMAX/4, 17*YMAX/32, f[vencedor].n);
+      readimagefile(f[vencedor].j, 3*XMAX/4, 20*YMAX/32, 7*XMAX/8, 27*YMAX/32);  
 
-  //não sei se é necessário um while aqui, provavelmente na versão final terá, porque os resultados estarão melhor analisados e mais complexos
-  cleardevice();
-  vencedor = apuracao(f.v[], 5);
-  outtextxy(XMAX/2, YMAX/2, "O vencedor é:");
-  outtextxy(XMAX/2, 17*YMAX/32, v.n[vencedor]);
-
+      setfillstyle(1,12);
+      setbkcolor(12);
+      bar(29*XMAX/32, 29*YMAX/32, 31*XMAX/32, 30*YMAX/32);
+      outtextxy(59*XMAX/64, 117*YMAX/128, "Estatisticas");
+      setbkcolor(15);
+    }
+    for(i=0; i < 6; i++)
+    {
+      porcentagemF[i] = ceil((100*f[i].v)/total);
+      porcentagemP[i] = ceil((100*g[i].v)/total);
+    }
+    for(i=0; i<6; i++)
+    {
+      pizzaF[i] = ceil((360*porcentagemF[i])/100);
+      pizzaP[i] = ceil((360*porcentagemP[i])/100);
+    }
+    while(tela==4)
+    {
+      setfillstyle(1,0);
+      pieslice(XMAX/4, YMAX/2, 0, pizzaF[0], 100);
+      pieslice(3*XMAX/4, YMAX/2, 0, pizzaP[0], 100);
+      for(i=1, c=1;i<6;i++, c++)
+        {
+          setfillstyle(1, c);
+          pieslice(XMAX/4, YMAX/2, pizzaF[i-1], pizzaF[i], 100);
+          pieslice(3*XMAX/4, YMAX/2, pizzaP[i-1], pizzaP[i], 100);
+        }
+    }
+  }
+  
   getchar();
   closegraph();
+
 }
 
-void clique(int *e, int *t, int *s, candidato f[])
+void clique(int *e, int *t, int *s, int *v)
 {
-  if(mousex() > primeirax1 && mousex() < primeirax2 && mousey() > primeiray1 && mousey() < primeiray2 && tela==1)
-  {
-    *e = 0;
-    outtextxy(XMAX/2,3*YMAX/4,"Voce escolheu Harry Potter e a Ordem da Fenix. Confirmar?");
-  }
-  if(mousex() > segundax1 && mousex() < segundax2 && mousey() > segunday1 && mousey() < segunday2 && tela==1)
-  {
-    *e = 1;
-    outtextxy(XMAX/2,3*YMAX/4,"Voce escolheu Gente Grande 2. Confirmar?"); 
-  }
-  if(mousex() > terceirax1 && mousex() < terceirax2 && mousey() > terceiray1 && mousey() < terceiray2 && tela==1)
-  {
-    *e = 2;
-      outtextxy(XMAX/2,3*YMAX/4,"Voce escolheu Exemplo3. Confirmar?");
-  }
-  if(mousex() > quartax1 && mousex() < quartax2 && mousey() > quartay1 && mousey() < quartay2 && tela==1)
-  {
-    *e = 3;
-    outtextxy(XMAX/2,3*YMAX/4,"Voce escolheu Exemplo4. Confirmar?");
-  }
-  if(mousex() > quintax1 && mousex() < quintax2 && mousey() > quintay1 && mousey() < quintay2 && tela==1)
-  {
-    *e = 4;
-    outtextxy(XMAX/2,3*YMAX/4,"Voce escolheu Exemplo5. Confirmar?");
-  }
-  if(mousex() > 7*XMAX/16 && mousex() < 9*YMAX/16 && mousey() > 9*XMAX/16 && mousey() < 10*YMAX/16 && tela==1)
-  {
-    *e = 5;
-    outtextxy(XMAX/2,3*YMAX/4,"Voce escolheu votar em branco. Confirmar?");
-  }
-  if(mousex() > 7*XMAX/16 && mousex() < 9*XMAX/16 && mousey() > 7*YMAX/16 && mousey() < 8*YMAX/16 && estado != -1 && tela==1)
-  {
-    if(estado == 5)
+  if(tela==1)
     {
-      *e = -1;
-      *t = total +1;
+    if(mousex() > primeirax1 && mousex() < primeirax2 && mousey() > primeiray1 && mousey() < primeiray2)
+    {
+      *e = 0;
+      apagapalavra();
+      outtextxy(XMAX/2,3*YMAX/4,"Voce escolheu personagem1. Confirmar?");
     }
-    else
+    if(mousex() > segundax1 && mousex() < segundax2 && mousey() > segunday1 && mousey() < segunday2)
     {
-      *f[estado] = f.v[estado] + 1;
-      *e = -1;
-      *t = total +1;
+      *e = 1;
+      apagapalavra();
+      outtextxy(XMAX/2,3*YMAX/4,"Voce escolheu personagem2. Confirmar?");
     }
-  }
-  if(mousex() > 29*XMAX/32 && mousex() < 31*XMAX/36 && mousey() > 29*YMAX/32 && mousey() < 30*YMAX/32 && tela==1)
+    if(mousex() > terceirax1 && mousex() < terceirax2 && mousey() > terceiray1 && mousey() < terceiray2)
     {
+      *e = 2;
+        apagapalavra();
+        outtextxy(XMAX/2,3*YMAX/4,"Voce escolheu personagem3. Confirmar?");
+    }
+    if(mousex() > quartax1 && mousex() < quartax2 && mousey() > quartay1 && mousey() < quartay2)
+    {
+      *e = 3;
+      apagapalavra();
+      outtextxy(XMAX/2,3*YMAX/4,"Voce escolheu personagem4. Confirmar?");
+    }
+    if(mousex() > quintax1 && mousex() < quintax2 && mousey() > quintay1 && mousey() < quintay2)
+    {
+      *e = 4;
+      apagapalavra();
+      outtextxy(XMAX/2,3*YMAX/4,"Voce escolheu personagem5. Confirmar?");
+    }
+    if(mousex() > 7*XMAX/16 && mousex() < 9*YMAX/16 && mousey() > 9*XMAX/16 && mousey() < 10*YMAX/16)
+    {
+      *e = 5;
+      apagapalavra();
+      outtextxy(XMAX/2,3*YMAX/4,"Voce escolheu votar em branco. Confirmar?");
+    }
+    if(mousex() > 7*XMAX/16 && mousex() < 9*XMAX/16 && mousey() > 7*YMAX/16 && mousey() < 8*YMAX/16 && estado != -1)
+    {
+      if(estado == 5)
+      {
+        *e = -1;
+      }
+      else
+      {
+        *v += 1;
+        *e = -1;
+      }
+      apagapalavra();
       *s = 2;
+    }   
+    if(mousex() > 29*XMAX/32 && mousex() < 31*XMAX/32 && mousey() > 29*YMAX/32 && mousey() < 30*YMAX/32)
+      {
+        *s = 3;
+      }  
+  }
+  if(tela==2)
+  {
+    if(mousex() > primeirax1 && mousex() < primeirax2 && mousey() > primeiray1 && mousey() < primeiray2)
+    {
+      *e = 0;
+      apagapalavra();
+      outtextxy(XMAX/2,3*YMAX/4,"Voce escolheu Harry Potter e a Ordem da Fenix. Confirmar?");
     }
+    if(mousex() > segundax1 && mousex() < segundax2 && mousey() > segunday1 && mousey() < segunday2)
+    {
+      *e = 1;
+      apagapalavra();
+      outtextxy(XMAX/2,3*YMAX/4,"Voce escolheu Gente Grande 2. Confirmar?");
+    }
+    if(mousex() > terceirax1 && mousex() < terceirax2 && mousey() > terceiray1 && mousey() < terceiray2)
+    {
+      *e = 2;
+        apagapalavra();
+        outtextxy(XMAX/2,3*YMAX/4,"Voce escolheu Exemplo3. Confirmar?");
+    }
+    if(mousex() > quartax1 && mousex() < quartax2 && mousey() > quartay1 && mousey() < quartay2)
+    {
+      *e = 3;
+      apagapalavra();
+      outtextxy(XMAX/2,3*YMAX/4,"Voce escolheu Exemplo4. Confirmar?");
+    }
+    if(mousex() > quintax1 && mousex() < quintax2 && mousey() > quintay1 && mousey() < quintay2)
+    {
+      *e = 4;
+      apagapalavra();
+      outtextxy(XMAX/2,3*YMAX/4,"Voce escolheu Exemplo5. Confirmar?");
+    }
+    if(mousex() > 7*XMAX/16 && mousex() < 9*YMAX/16 && mousey() > 9*XMAX/16 && mousey() < 10*YMAX/16)
+    {
+      *e = 5;
+      apagapalavra();
+      outtextxy(XMAX/2,3*YMAX/4,"Voce escolheu votar em branco. Confirmar?");
+    }
+    if(mousex() > 7*XMAX/16 && mousex() < 9*XMAX/16 && mousey() > 7*YMAX/16 && mousey() < 8*YMAX/16 && estado != -1)
+    {
+      if(estado == 5)
+      {
+        *e = -1;
+        *t = total +1;
+      }
+      else
+      {
+        *v += 1;
+        *e = -1;
+        *t = total +1;
+      }
+      apagapalavra();
+      *s = 1
+    }
+  }
+  if(tela==3)
+  {
+    if(mousex() > 29*XMAX/32 && mousex() < 31*XMAX/32 && mousey() > 29*YMAX/32 && mousey() < 30*YMAX/32)
+    {
+      *s = 4;
+    }    
+  }
 }
 
-int apuracao(int f[], int tamanho)
+int apuracao(candidato f[], int tamanho)
 {
     int maior, indice, i;
-    maior = f[0];
+    maior = f[0].v;
     indice = 0;
     for (i=1; i < tamanho; i++)
     {
-        if (f[i] > maior)
+        if (f[i].v > maior)
         {
-            maior = f[i];
+            maior = f[i].v;
             indice = i;
         }
     }
     return indice;
+}
+
+void apagapalavra(int tamanhodapalavra)
+{
+    setfillstyle(1,15);
+    bar(XMAX/2,3*YMAX/4,(XMAX/2)+480, (3*YMAX/4)+16);
 }
